b4058b06af95cd6f7e94fb9b24d303acae49c888
houjian99 <houjian6@gmail.com>
2015-05-21 15:22:35 +0800

    creat js
    itinerary json

---
+++ itinerary.js
@@ -0,0 +1,6 @@
+var itinerarys= [
+{ "itineraryName":"巴黎市区一日游" , "itineraryHerf":"http://www.ipiaoling.com/tour/paris-one-day/" },
+{ "itineraryName":"巴黎王宫乡村一日游" , "itineraryHerf":"http://www.ipiaoling.com/tour/palais-village/" },
+{ "itineraryName":"圣米歇尔山2日游" , "itineraryHerf":"http://www.ipiaoling.com/tour/saint_michel_2day/" },
+{ "itineraryName":"巴黎+普罗旺斯+蔚蓝海岸 10天" , "itineraryHerf":"http://www.ipiaoling.com/tour/france_10/" }
+]
