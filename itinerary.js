98948c7f1d3237f4df0a096cc4893ab9cdea1127
houjian99 <houjian6@gmail.com>
2015-05-26 18:09:35 +0800

    updata all le destitation

--- itinerary.js
+++ itinerary.js
@@ -2,5 +2,18 @@ var itinerarys= [
 { "itineraryName":"巴黎市区一日游" , "itineraryHerf":"http://www.ipiaoling.com/tour/paris-one-day/" },
 { "itineraryName":"巴黎王宫乡村一日游" , "itineraryHerf":"http://www.ipiaoling.com/tour/palais-village/" },
 { "itineraryName":"圣米歇尔山2日游" , "itineraryHerf":"http://www.ipiaoling.com/tour/saint_michel_2day/" },
-{ "itineraryName":"巴黎+普罗旺斯+蔚蓝海岸 10天" , "itineraryHerf":"http://www.ipiaoling.com/tour/france_10/" }
+{ "itineraryName":"巴黎+普罗旺斯+蔚蓝海岸 10天" , "itineraryHerf":"http://www.ipiaoling.com/tour/france_10/" },
+{ "itineraryName":"蔚蓝海岸线路与普罗旺斯五天四夜自由行" , "itineraryHerf":"http://www.ipiaoling.com/tour/cote_dazur_5day/" },
+{ "itineraryName":"大峡谷Valensole圣十字湖·一日游" , "itineraryHerf":"http://www.ipiaoling.com/tour/valensole_saint_crois/" },
+{ "itineraryName":"薰衣草峡谷漂流探险游.2天1晚" , "itineraryHerf":"http://www.ipiaoling.com/tour/lavender_tour_2days/" },
+{ "itineraryName":"普罗旺斯经典小镇之旅" , "itineraryHerf":"http://www.ipiaoling.com/tour/provence-village/" },
+{ "itineraryName":"阿尔勒-向日葵之旅" , "itineraryHerf":"http://www.ipiaoling.com/tour/arles-tournesol/" },
+{ "itineraryName":"波尔多酒庄文化之旅" , "itineraryHerf":"http://www.ipiaoling.com/tour/borteau_vin_voyage/" },
+{ "itineraryName":"波尔多「将爱」之旅" , "itineraryHerf":"http://www.ipiaoling.com/tour/bordeaux-jiangai/" },
+{ "itineraryName":"比利时荷兰周末赏花之旅·3天2晚" , "itineraryHerf":"http://www.ipiaoling.com/tour/belgui_paybas_3days/" },
+{ "itineraryName":"比利时荷兰2天1夜周末游" , "itineraryHerf":"http://www.ipiaoling.com/tour/bilishi_helan_2-1/" },
+{ "itineraryName":"卢瓦尔河谷城堡梦幻游" , "itineraryHerf":"http://www.ipiaoling.com/tour/chateaux-de-loir/" },
+{ "itineraryName":"世界遗产文化路线" , "itineraryHerf":"http://www.ipiaoling.com/tour/world_cultural_routes/" }
+
+
 ]
